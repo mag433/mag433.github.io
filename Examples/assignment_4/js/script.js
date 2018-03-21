0696d9202a01c6fb68aaa1b66faa6111cee3de80
mag433 <mag433@miami.edu>
2018-03-21 13:47:06 -0400

    a

---
+++ Examples/assignment_4/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+ //Hide stuff
+
+  $('#getStarted').hide();
+
+  //alert("hello");
+  $('#title').on('mouseover',function(){
+      $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+      $('#getStarted').show();
+    });
+  $('#title').on('mouseleave',function(){
+      $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+
+    });
+
+    $('#next').on('click',function(){
+
+      var currentItem = $('li.active');
+      var nextItem = $('li.active').next();
+
+      currentItem.toggleClass('active');
+      nextItem.toggleClass('active');
+
+    });
+});
