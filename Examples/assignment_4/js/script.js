9b8de0fc6a9c8e7d7ae64aa286ffb7e74dd6ea06
mag433 <mag433@miami.edu>
2018-03-29 11:05:00 -0400

    s

--- Examples/assignment_4/js/script.js
+++ Examples/assignment_4/js/script.js
@@ -1,25 +1,98 @@
 $(function(){
- //Hide stuff
-
-  $('#getStarted').hide();
-
+  //Hide stuff
+  $('#getStarted, #finish, #previous, .hide, #survey').hide();
   //alert("hello");
   $('#title').on('mouseover',function(){
-      $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
-      $('#getStarted').show();
-    });
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+    $('#getStarted').show();
+  });
+
   $('#title').on('mouseleave',function(){
-      $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+    $('.fa-arrow-alt-circle-left').toggleClass('fa-arrow-alt-circle-right');
+  });
 
+  $('#getStarted').on('click', function(){
+    $('.hide').show();
+    $('#getStarted').hide();
+  });
+  $('#finish').on('click',function(){
+    $('.hide, #next, #previous, #finish').hide();
+    $('#survey').show();
+
+  });
+
+
+//survey
+$('#yes').on('click',function(){
+  $('#surveytitle').css("background-color","green");
+  $('#survey').css({
+    "border":"1px solid green",
+    "background":"#ccc"
+  });
     });
-
-    $('#next').on('click',function(){
-
-      var currentItem = $('li.active');
-      var nextItem = $('li.active').next();
-
-      currentItem.toggleClass('active');
-      nextItem.toggleClass('active');
-
+  $('#no').on('click',function(){
+    $('#surveytitle').css("background-color","red");
+    $('#survey').css({
+      "border":"1px solid green",
+      "background":"#FFFFFF"
     });
 });
+
+  $('#next').on('click',function(){
+
+    var currentItem = $('li.active');
+
+    var nextItem = $('li.active').next();
+
+    currentItem.toggleClass('active');
+
+    nextItem.toggleClass('active');
+
+    // Conditional eval.
+    if($('li').last().hasClass('active')) {
+      $('#next').hide();
+      $('#finish').show();
+    } else {
+      $('#next').show();
+    }
+
+    // Conditional eval.
+    if($('li').first().hasClass('active')) {
+      $('#previous').hide();
+    } else {
+      $('#previous').show();
+
+    }
+
+
+  });
+
+  $('#previous').on('click',function(){
+    //First, we need to keep track of the current slide and the next slide. We achieve this by storing our elements in variables.
+    var currentItem = $('li.active');
+    //Position based selectors:
+    // first() https://api.jquery.com/first/
+    // last() https://api.jquery.com/last/
+    // prev() Documentation https://api.jquery.com/prev/
+    // next() Documentation https://api.jquery.com/next/
+    var previousItem = $('li.active').prev();
+
+    currentItem.toggleClass('active');
+
+    previousItem.toggleClass('active');
+
+    // Conditional eval.
+    if($('li').last().hasClass('active')) {
+      $('#next').hide();
+      $('#finish').show();
+    } else {
+      $('#next').show();
+    }
+    // Conditional eval.
+    if($('li').first().hasClass('active')) {
+      $('#previous').hide();
+    } else {
+      $('#previous').show();
+    }
+      });
+        });
